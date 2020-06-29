2b1380cec767d3eef2d020b103fa3adf4bee5c8f
Mr Footwork <pandau.ting@gmail.com>
2020-06-29 21:07:50 +0200

    Currying: eine gute Callback Übung
    Callbacks fange ich an zu verstehen. Aber Promise-Handling ist noch nicht ganz greifbar. Funktionen mit den Promise-Methoden durchreichen ist noch nicht wirklich intuitiv für mich.

--- functional.js
+++ functional.js
@@ -1,13 +1,5 @@
-/**Promises III: Native Promise, Introducing the Executor */
-let promise = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve('edabit')
-	}, 1000)
-})
-// console.log(promise)
-
 /** Promises II: What Is a Callback?*/
-function anotherFunc(manipulator){
+function anotherFunc(manipulator) {
 	let str = 'bye'
 	setTimeout(() => {
 		manipulator(str)
@@ -17,9 +9,9 @@ function anotherFunc(manipulator){
 
 var doc = 'hello'
 
-function callback(str){
-	doc = str
-}
+// function callback(str) {
+// 	doc = str
+// }
 // console.log('before callback: ', doc)
 // console.log("callback('test') running...", callback('test'))
 // console.log('after callback: ', doc)
@@ -30,3 +22,111 @@ function callback(str){
 // setTimeout(() => {
 // 	console.log('after waiting: ', doc)
 // }, 100)
+
+/**Promises III: Native Promise, Introducing the Executor */
+let promiseIII = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve('edabit')
+	}, 1000)
+})
+// console.log(promise)
+
+/**Promises IV: The Reject Callback */
+let promiseIfDog = new Promise((resolve, reject) => {
+	let animal = 'cat'
+	setTimeout(() => {
+		if (animal === 'dog') {
+			resolve("It's a dog!")
+		}
+		if (animal !== 'dog') {
+			reject("It's not a dog!")
+		}
+	}, 1000)
+}).catch(e => {
+	console.error(e)
+})
+// console.log(promiseIfDog)
+
+/**Promises V: Then */
+var result = ''
+
+let promiseV = new Promise(resolve => resolve('success!'))
+
+// promiseV.then(val => (result = val))
+// console.log(result)
+
+/** Promises VI: Catch*/
+let errorLog = {}
+
+let promiseVI = new Promise(() => {
+	throw new Error('Something failed')
+}).catch(error => (errorLog = error))
+
+/**Promises VII: Chaining */
+function doSomething(isGoingToResolve = true) {
+	return new Promise((resolve, reject) => {
+		if (isGoingToResolve) {
+			resolve('something')
+		}
+		else {
+			reject('something else')
+		}
+	})
+		.then(response => {
+			console.log('in my function', response)
+			return response
+		})
+		.catch(error => {
+			console.log('in my function', error)
+		})
+}
+
+// doSomething().then(response => {
+// 	console.log('in my main call', response)
+// })
+
+/**Promises VIII: Old Callback API */
+function saySomething(str) {
+	throw 'what the heck'
+}
+let ERR = ''
+const wait = () => new Promise(resolve => setTimeout(resolve, 1000))
+wait().then(() => saySomething('1 second has passed')).catch(error => (ERR = error))
+
+/**Promises IX: Composition Introduction */
+var resultIX = ''
+var thenable = {
+	then : function(onFulfill, onReject) {
+		onFulfill('edabit!')
+	}
+}
+var promise = Promise.resolve(thenable).then(passed => (resultIX = passed))
+
+/**Anonymous Functions All The Way Down */
+const lambdaDepth = n => eval('() => '.repeat(n) + '"edabit"')
+// console.log(lambdaDepth(1))
+// console.log(lambdaDepth(2))
+// console.log(lambdaDepth(3))
+// console.log(lambdaDepth(0))
+
+/**Don't Be in Such a Curry */
+let carLot = {}
+const carMaker = make => carCount => (carLot[make] = (carLot[make] || 0) + carCount)
+const toyotaCars = carMaker('Toyota')
+const benzCars = carMaker('Mercedes')
+// console.log('toyotaCars', toyotaCars(2))
+// console.log('benzCars', benzCars(3))
+// console.log('carLot', carLot)
+// console.log('benzCars', benzCars(1))
+// console.log('carLot', carLot)
+
+/**Currying */
+add5 = num => num + 5
+add6 = num => num + 6
+// console.log(add5(2))
+// console.log(add6(3))
+const addAny = num => add => add + num
+const addFive = addAny(5)
+const addSix = addAny(6)
+// console.log(addFive(2))
+// console.log(addSix(1))
