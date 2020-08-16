733f40b852d261687c827d742caec21fac5a70a5
Mr Footwork <pandau.ting@gmail.com>
2020-08-16 18:36:25 +0200

    Data Structure Exercise

--- data.js
+++ data.js
@@ -112,3 +112,21 @@ function twins(arr) {
 	return 0
 }
 // console.log(twins([ 10, 1, 2, 3, 5, 5 ]))
+
+/**Album in Albumns */
+function releaseYear(album) {
+	const albums = {
+		'2015' : [ 'Vulnicura', 'Honeymoon', 'Rebel Heart' ],
+		'2016' : [ 'Lemonade', 'Blackstar', 'A Moon Shaped Pool' ],
+		'2017' : [ 'Flower Boy', 'Antisocialites' ],
+		'2018' : [ 'El Mal Querer', 'Someone Out There', 'Cranberry', 'Kamikaze' ],
+		'2019' : [ 'thank u next', 'Magdalene', 'Ode to Joy' ],
+		'2020' : [ 'Rough and Rowdy Ways', 'folklore', 'Future Nostalgia', 'Colores' ],
+	}
+	let search = Object.keys(albums).filter(year => albums[year].includes(album))[0]
+	return search ? +search : 'Unknown'
+}
+// console.log(releaseYear('Ode to Joy'))
+// console.log(releaseYear('Honeymoon'))
+// console.log(releaseYear('fake'))
+// console.log(releaseYear('thank u next'))
